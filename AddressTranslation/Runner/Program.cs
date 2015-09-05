a5c8041e65b2f8bfbb5e579d5ed56f813b87471b
martin <diaz.martin06@gmail.com>
2015-09-05 13:09:47 -0300

    first comit with base project

---
+++ AddressTranslation/Runner/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using GoogleApiRequest;
+using GoogleEntities;
+
+namespace Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string addressNumber = "5900";
+                string streetName = "Rivadavia";
+                string cityName = "Buenos Aires";
+                string countryName = "AR";
+
+                GeocodeResponse googleResponse = GoogleConnector.MakeRequest(addressNumber, streetName, cityName.Replace(" ", "+"), countryName); // entry point, replace hard coded string with params
+                ProcessResponse(googleResponse);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.Read();
+            }
+
+        }
+
+        static public void ProcessResponse(GeocodeResponse googleResponse)
+        {
+            string Status = googleResponse.status;
+            //googleResponse.results[0].types[0];
+            Console.WriteLine(googleResponse.results[0].types[0]);
+            Console.WriteLine(googleResponse.results[0].geometry.location.lat);
+            Console.WriteLine(googleResponse.results[0].geometry.location.lng);
+        }
+    }
+}
+
+
